c9ed63359ba847da0199035a7109954babf52ec7
mirzakamran <62909581+mirzakamran@users.noreply.github.com>
2020-06-01 19:07:32 -0400

    implicit wait updated

--- src/com/rogers/test/base/BaseTestClass.java
+++ src/com/rogers/test/base/BaseTestClass.java
@@ -245,7 +245,7 @@ public class BaseTestClass {
  		default :
  			captcha_bypass_handlers.captchaBypassURLLoginFlows(strUrl, strLanguage);
 		}
-	    setImplicitWait(getDriver(), 10);
+	    setImplicitWait(getDriver(), 120);
 	    init(strGroupName);	 
  }
 		
