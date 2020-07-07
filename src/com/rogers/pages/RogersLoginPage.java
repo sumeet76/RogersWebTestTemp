0cc7222e54684f0062721fb6f9c2a5f2addc5c55
mirzakamran <62909581+mirzakamran@users.noreply.github.com>
2020-07-07 16:48:01 -0400

    TC91

--- src/com/rogers/pages/RogersLoginPage.java
+++ src/com/rogers/pages/RogersLoginPage.java
@@ -63,6 +63,14 @@ public class RogersLoginPage extends BasePageClass {
 	}
 	
 	/**
+	 * Is sign in frame displayed
+	 * @author Mirza.Kamran
+	 */
+	 public boolean isSignInFrameDisplayed() {
+		 return reusableActions.isElementVisible(fraSignIn);
+	 }
+	
+	/**
 	 * Enter the user name on Sign in frame
 	 * @param strUsername user name to be login
 	 * @author chinnarao.vattam
