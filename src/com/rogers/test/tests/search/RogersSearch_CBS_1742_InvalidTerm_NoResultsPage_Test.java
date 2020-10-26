bfe198eed24f37014c61f1b60c3eeb56043a9d65
Rajesh Varalli <Rajesh.Varalli1@rci.rogers.com>
2020-10-25 20:25:15 -0400

    3 new tests 1732, 1734, 1742 added

---
+++ src/com/rogers/test/tests/search/RogersSearch_CBS_1742_InvalidTerm_NoResultsPage_Test.java
@@ -0,0 +1,68 @@
+package com.rogers.test.tests.search;
+
+import com.rogers.test.base.BaseTestClass;
+import com.rogers.test.helpers.RogersEnums;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+import utils.CSVReader;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+
+public class RogersSearch_CBS_1742_InvalidTerm_NoResultsPage_Test extends BaseTestClass {
+    @DataProvider(name = "FilterData")
+    public Object[] testData() throws IOException
+    {
+        String csvFileName = System.getProperty("user.dir") + "/test-data/rogers/search/InvalidTerms.csv";
+        List<String[]> csvData = CSVReader.parseCsvData(csvFileName);
+        Object[] csvRow = new Object[csvData.size()];
+
+        for(int i =0; i < csvData.size();i++){
+            csvRow[i] = csvData.get(i);
+        }
+
+        return csvRow;
+    }
+
+    @Test(dataProvider = "FilterData")
+    public void validateResultLandingPage(String[] csvRow) throws UnsupportedEncodingException {
+        getDriver().get(System.getProperty("SearchUrl"));
+        reporter.reportLogWithScreenshot("CBS Search Page");
+        rogers_search_page.enterTextSearch(csvRow[0]);
+        reporter.reportLogWithScreenshot("Search field entered");
+        reporter.hardAssert(rogers_search_page.isSuggestionsSectionDisplayed(),
+                "Suggestion Label Visible", "Suggestion Label Not Visible");
+        reporter.hardAssert(rogers_search_page.isSupportSectionDisplayed(),
+                "Support Label Visible", "Support Label Not Visible");
+        reporter.hardAssert(!rogers_search_page.isSupportSectionPopulated(),
+                "Suggestions not loaded", "Suggestion loaded");
+        reporter.hardAssert(!rogers_search_page.isLeftSectionPopulated(),
+                "Support links not loaded", "Support links loaded");
+        rogers_search_page.clkOnMagnifyingLens();
+        reporter.reportLogWithScreenshot("Search Results Page");
+        reporter.hardAssert(rogers_search_page.validateResultLandingPageURL(csvRow[0]),
+                "Result Landing Page displayed", "Result Landing Page not displayed");
+        reporter.hardAssert(rogers_search_page.isNoResultsDisplayed(),
+                "No Results Found","No Results message not displayed");
+        reporter.hardAssert(!rogers_search_page.isFilterDisplayed("Support"),
+                "Filter Support is Not Displayed", "Filter Support is Displayed");
+        reporter.hardAssert(!rogers_search_page.isFilterDisplayed("Shop"),
+                "Filter Shop is Not Displayed", "Filter Shop is Displayed");
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"strBrowser", "strLanguage"})
+    public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage, ITestContext testContext, Method method) throws IOException {
+        xmlTestParameters = new HashMap<String, String>(testContext.getCurrentXmlTest().getAllParameters());
+        startSession(System.getProperty("SearchUrl") + "wireless", strBrowser, strLanguage, RogersEnums.GroupName.search, method);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterTest() {
+        closeSession();
+    }
+
+}
