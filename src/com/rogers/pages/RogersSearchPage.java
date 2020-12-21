e07cf897fe83dcd895a0f3fdba0e061fcb6b4c2a
Dimpy Gupta <Dimpy.Gupta@rci.rogers.com>
2020-12-21 11:37:16 -0500

    no message

--- src/com/rogers/pages/RogersSearchPage.java
+++ src/com/rogers/pages/RogersSearchPage.java
@@ -312,6 +312,7 @@ public class RogersSearchPage extends BasePageClass {
     public void clkShopAndThenWirelessFilter() {
 
         getReusableActionsInstance().clickWhenReady(By.xpath("//button[@id='Shop-heading-1']"));
+        getReusableActionsInstance().staticWait(500);
       //  getReusableActionsInstance().clickWhenReady(By.xpath("//p[starts-with(text(),'Shop')]/ancestor::button//following-sibling::ds-expander//ds-accordion-panel[contains(@class,'-sub-level')]/div/button/div/div/p[starts-with(text(),'Wireless')]"));
         getReusableActionsInstance().clickWhenReady(By.xpath("//button[@id='Wireless-heading-4']"));
 		/*WebElement parentFilter = getDriver().findElement(By.xpath("//ds-accordion-panel[contains(@class,'-main-level')]/div/button//p[starts-with(text(),'"
