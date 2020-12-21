b7e7af109886ab0a4690a1b2ccf8e5fd2d457341
Dimpy Gupta <Dimpy.Gupta@rci.rogers.com>
2020-12-21 15:00:58 -0500

    no message

--- src/com/rogers/pages/RogersSearchPage.java
+++ src/com/rogers/pages/RogersSearchPage.java
@@ -311,10 +311,10 @@ public class RogersSearchPage extends BasePageClass {
 
     public void clkShopAndThenWirelessFilter() {
 
-        getReusableActionsInstance().clickWhenReady(By.xpath("//button[@id='Shop-heading-1']"));
+        getReusableActionsInstance().clickWhenReady(By.xpath("//button[contains(@id,'Shop-heading')]"));
         getReusableActionsInstance().staticWait(500);
       //  getReusableActionsInstance().clickWhenReady(By.xpath("//p[starts-with(text(),'Shop')]/ancestor::button//following-sibling::ds-expander//ds-accordion-panel[contains(@class,'-sub-level')]/div/button/div/div/p[starts-with(text(),'Wireless')]"));
-        getReusableActionsInstance().clickWhenReady(By.xpath("//div[contains(@id,'Shop-body-1')]/div/button[contains(@id,'Wireless-heading')]"));
+        getReusableActionsInstance().clickWhenReady(By.xpath("//div[contains(@id,'Shop-body')]/div/button[contains(@id,'Wireless-heading')]"));
 		/*WebElement parentFilter = getDriver().findElement(By.xpath("//ds-accordion-panel[contains(@class,'-main-level')]/div/button//p[starts-with(text(),'"
 				+ strFilterName + "')]"));
 
