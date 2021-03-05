a9874fed725ed58836356a0967afa3271bb3ebe5
ChinnaraoVattam <chinnarao.vattam@rci.rogers.com>
2021-03-04 20:09:09 -0500

    Added new scripts

---
+++ src/com/rogers/test/tests/connectedhome/desktop/RogersCH_TC_065_Regression_SAI_ISS_NAC_AnonymousCxentersInternetShopPagefromISSlearnpage_ISStogglecheckedTest.java
@@ -0,0 +1,129 @@
+package com.rogers.test.tests.connectedhome.desktop;
+import com.rogers.test.base.BaseTestClass;
+import com.rogers.test.helpers.RogersEnums;
+import com.rogers.testdatamanagement.TestDataHandler;
+import org.apache.http.client.ClientProtocolException;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+
+/**
+ * This class contains the test method to test Legacy Internet Offer Buy flow for Rogers.com
+ *
+ * @author chinnarao.vattam
+ *
+ * Test steps:
+ * 1.Launch Rogers SAI Tupelo URL in QA Env and click on get it now and enter address and click on continue
+ * 2. Click on continue
+ * 3. Choose Internet, SmartStream checkbox and click on Load offers
+ * 4. Add 1 STB and click on Add to cart
+ * 5. Click on Continue
+ * 6. Click on Checkout
+ * 7. Click on Yes, continue
+ * 8. Click on Continue
+ * 9. Enter DOB, valid ID details and click on continue
+ * 10. Choose Installation type as enhanced self install and click on continue
+ * 11. Click on Continue
+ * 12. Click on Submit
+ *
+ **/
+
+public class RogersCH_TC_065_Regression_SAI_ISS_NAC_AnonymousCxentersInternetShopPagefromISSlearnpage_ISStogglecheckedTest extends BaseTestClass {
+
+    @Test(groups = {"RegressionCH","SmartStreamCH"})
+    public void checkSaiTupeloBuyflowAddAdditionalSTB() {
+        getDriver().navigate().to(TestDataHandler.rogersConfig.getRogersURL()+"/web/consumer/internet/streaming");
+        reporter.reportLogWithScreenshot("Launched the Stream Availability check page");
+        getRogersInternetPackageSelectionPage().clkSmartStreamAvailability() ;
+        String  strAddressLine1=TestDataHandler.tc01_02_03_IgniteTVAccount.getAccountDetails().getAddress().get("line1");
+        String  strAddressLine2=TestDataHandler.tc01_02_03_IgniteTVAccount.getAccountDetails().getAddress().get("line2");
+        getRogersHomePage().setIgniteAddressLookup(strAddressLine1+", "+strAddressLine2+", CANADA");
+        getRogersHomePage().clkIgniteAddressLookupSubmitSS();
+        reporter.reportLogWithScreenshot("Serviceability check popup has displayed to check the Service availability");
+        getRogersInternetPackageSelectionPage().clkSmartStreamPackage();
+        getRogersIgniteTVBuyPage().clkPlusAddIgniteTVBoxes();
+        getRogersIgniteTVBuyPage().clkUpdateCart();
+        getRogersInternetPackageSelectionPage().clkInternetBuyContinue();
+
+        reporter.hardAssert(getRogersInternetProfilePage().verifyProfilePageSAI(),"Profile page has Launched","Profile page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the create profile page");
+        getRogersInternetProfilePage().setEmail();
+        getRogersInternetProfilePage().setFirstname();
+        getRogersInternetProfilePage().setLastName();
+        getRogersInternetProfilePage().setPhone();
+        getRogersInternetProfilePage().clkSubmitProfile();
+
+        reporter.hardAssert(getRogersInternetCreditCheckPage().verifyCreditEvalutionPage(),"Credit Evalution page has Launched","Credit Evalution page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the credit evalution page");
+        getRogersInternetCreditCheckPage().selectDOBYear();
+        getRogersInternetCreditCheckPage().selectDOBMonth();
+        getRogersInternetCreditCheckPage().selectDOBDay();
+        reporter.reportLogWithScreenshot("Entered DOB details");
+        getRogersInternetCreditCheckPage().selectFirstID("Driver's License");
+        getRogersInternetCreditCheckPage().selectProvince("Ontario");
+        getRogersInternetCreditCheckPage().selectExpiryYear();
+        getRogersInternetCreditCheckPage().selectExpiryMonth();
+        getRogersInternetCreditCheckPage().selectExpiryDay();
+        getRogersInternetCreditCheckPage().setDrivingLicenseNumber("ONTARIO");
+        reporter.reportLogWithScreenshot("Driving License Details");
+        getRogersInternetCreditCheckPage().selectSecondIDOption("Passport");
+        getRogersInternetCreditCheckPage().setPassportNumber();
+        getRogersInternetCreditCheckPage().selectPassportExpiryYear();
+        getRogersInternetCreditCheckPage().selectPassportExpiryMonth();
+        getRogersInternetCreditCheckPage().selectPassportExpiryDay();
+        getRogersInternetCreditCheckPage().clkCreditConsentSai();
+        reporter.reportLogWithScreenshot("Passport Details");
+        getRogersInternetCreditCheckPage().clkCreditConsentSubmit();
+
+        reporter.hardAssert(getRogersTechInstallPage().verifyTechInstallPage(),"TechInstall page has Launched","TechInstall page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the tech install page");
+        getRogersTechInstallPage().clkTechInstalConsent();
+        reporter.reportLogWithScreenshot("tech install details");
+        getRogersTechInstallPage().clkTechInstallContinue();
+
+        reporter.hardAssert(getRogersPaymentOptionsPage().verifyPaymentModepage(),"Payment Mode page has Launched","Payment Mode page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the payment options page");
+        getRogersPaymentOptionsPage().selectPaymentMode("Pre-authorized Credit Card");
+        getRogersPaymentOptionsPage().switchToCreditCardIFrame();
+        getRogersPaymentOptionsPage().setCreditCardNumberIFrame(TestDataHandler.chPaymentInfo.getCreditCardDetails().getNumber());
+        getRogersPaymentOptionsPage().switchOutOfCreditCardIFrame();
+        getRogersPaymentOptionsPage().setCVV();
+        getRogersPaymentOptionsPage().selectExpiryMonth();
+        getRogersPaymentOptionsPage().selectExpiryYear();
+        reporter.reportLogWithScreenshot("Payment options Details");
+        getRogersPaymentOptionsPage().clkPaymentConfirm();
+
+        reporter.hardAssert(getRogersOrderReviewPage().verifyAgreementPage(),"Agreement page has Launched","Agreement page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the order review page");
+
+        reporter.hardAssert( getRogersOrderReviewPage().verifyAgreement(),"Agreement has Launched","Agreement has not Launched");
+        getRogersOrderReviewPage().clkAcceptenceCheckbox();
+        reporter.reportLogWithScreenshot("Agreement details");
+        getRogersOrderReviewPage().clkSubmit();
+        reporter.reportLogWithScreenshot("Launched the Confirmation page");
+        reporter.hardAssert(getRogersOrderConfirmationPage().verifyOrderConfirmationNew(),"Order has created successfully","Order has failed");
+        reporter.reportLogWithScreenshot("Launched the Confirmation page");
+    }
+
+
+    @BeforeMethod (alwaysRun=true) @Parameters({ "strBrowser", "strLanguage"})
+    //legacyAnonymous
+    public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage, ITestContext testContext,Method method) throws ClientProtocolException, IOException {
+        startSession(System.getProperty("QaUrl"),  strBrowser,strLanguage,RogersEnums.GroupName.connectedhome_igniteanonymous, method);
+        // xmlTestParameters = new HashMap<String, String>(testContext.getCurrentXmlTest().getAllParameters());
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void afterTest() {
+        closeSession();
+    }
+
+
+
+}
+
+
