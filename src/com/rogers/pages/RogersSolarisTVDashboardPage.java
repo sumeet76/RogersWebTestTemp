d8e3b2471c008d11b0ac389a8d0aa27b6e36116a
ChinnaraoVattam <chinnarao.vattam@rci.rogers.com>
2021-04-25 22:47:05 -0400

    Updated page objects

--- src/com/rogers/pages/RogersSolarisTVDashboardPage.java
+++ src/com/rogers/pages/RogersSolarisTVDashboardPage.java
@@ -264,7 +264,7 @@ public class RogersSolarisTVDashboardPage extends BasePageClass {
 	public void clkChangeTVPackage() {
 		//NL and FL  Provinces taking long loading time to pull the dashboard details
 		getReusableActionsInstance().staticWait(6000);
-		getReusableActionsInstance().waitForElementVisibility(btnChangeTVPackage,120);
+		//getReusableActionsInstance().waitForElementVisibility(btnChangeTVPackage,120);
 		//getReusableActionsInstance().executeJavaScriptClick(btnChangeTVPackage);
 		getReusableActionsInstance().getWhenReady(btnChangeTVPackage, 60).click();
 	}
