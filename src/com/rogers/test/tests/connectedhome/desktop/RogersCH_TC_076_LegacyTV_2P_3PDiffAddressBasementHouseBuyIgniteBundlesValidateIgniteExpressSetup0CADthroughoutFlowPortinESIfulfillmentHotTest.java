1f19a891e389f36fecdb9239a4e1400770f94481
ChinnaraoVattam <chinnarao.vattam@rci.rogers.com>
2021-05-13 15:56:05 -0400

    Updated DB Validation for the possible scripts

--- src/com/rogers/test/tests/connectedhome/desktop/RogersCH_TC_076_LegacyTV_2P_3PDiffAddressBasementHouseBuyIgniteBundlesValidateIgniteExpressSetup0CADthroughoutFlowPortinESIfulfillmentHotTest.java
+++ src/com/rogers/test/tests/connectedhome/desktop/RogersCH_TC_076_LegacyTV_2P_3PDiffAddressBasementHouseBuyIgniteBundlesValidateIgniteExpressSetup0CADthroughoutFlowPortinESIfulfillmentHotTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * This class contains the test method to verify the upgarde flow for Legacy TV to IgniteTV for Rogers.com  
@@ -146,6 +147,17 @@ public class RogersCH_TC_076_LegacyTV_2P_3PDiffAddressBasementHouseBuyIgniteBund
 		reporter.reportLogWithScreenshot("Launched the Confirmation page");
 		reporter.hardAssert(getRogersOrderConfirmationPage().verifyOrderConfirmationNew(),"Order has created successfully","Order has failed");
 		reporter.reportLogWithScreenshot("Launched the Confirmation page");
+		String ban = getRogersOrderConfirmationPage().getBAN();
+		System.out.println("BAN from the portal : " + ban);
+		/**
+		 * DB Validations in the subscriber table
+		 */
+
+		Map<Object, Object> dblists = getDbConnection().connectionMethod(System.getProperty("DbEnvUrl"))
+				.executeDBQuery("select BAN,ACCOUNT_SUB_TYPE,SYS_CREATION_DATE from billing_account where BAN='" + ban + "'", false);
+
+		reporter.softAssert(dblists.get("BAN").equals(ban),"Entry is updated in the billing table","BAN is not present in the billing account table");
+		reporter.softAssert(dblists.get("ACCOUNT_SUB_TYPE").equals("R"),"ACCOUNT_SUB_TYPE is verified as R","Account type is not updated as R");
     	}
 
 	@BeforeMethod (alwaysRun=true) @Parameters({ "strBrowser", "strLanguage"})
