b9fb285537a97029808462796a9f032154d00659
Rajesh Varalli <Rajesh.Varalli1@rci.rogers.com>
2021-05-27 13:42:30 -0400

    reporter object fixed in DBValidation

--- src/com/rogers/test/helpers/DBValidation.java
+++ src/com/rogers/test/helpers/DBValidation.java
@@ -36,7 +36,7 @@ public class DBValidation extends BaseTestClass{
             statement.close();
             connect.close();
         } catch (SQLException e) {
-            new BaseTestClass().reporter.reportLog("Exception occurred while executing the Query, please " +
+            reporter.reportLog("Exception occurred while executing the Query, please " +
                     "check the query" + e.getMessage());
             e.printStackTrace();
         }
@@ -68,7 +68,8 @@ public class DBValidation extends BaseTestClass{
             connect.setAutoCommit(false);
             statement = connect.createStatement();
         } catch (ClassNotFoundException | SQLException e) {
-            new BaseTestClass().reporter.reportLog("DB Connection error" + e.getMessage());
+            reporter.reportLog("DB Connection error" + e.getMessage());
+            e.printStackTrace();
         }
         return this;
     }
