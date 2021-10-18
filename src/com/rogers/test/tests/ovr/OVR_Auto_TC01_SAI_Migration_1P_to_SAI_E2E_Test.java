451a93c6c1d823746ad813e98ad0eb691990f40b
Sam-Ahuja <sameer.ahuja@rci.rogers.ca>
2021-10-18 12:33:12 -0400

    Modified Test Case

---
+++ src/com/rogers/test/tests/ovr/OVR_Auto_TC01_SAI_Migration_1P_to_SAI_E2E_Test.java
@@ -0,0 +1,78 @@
+package com.rogers.test.tests.ovr;
+
+import com.rogers.test.base.BaseTestClass;
+import com.rogers.test.helpers.RogersEnums;
+import com.rogers.testdatamanagement.TestDataHandler;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+import utils.FormFiller;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class OVR_Auto_TC01_SAI_Migration_1P_to_SAI_E2E_Test extends BaseTestClass {
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"strBrowser", "strLanguage"})
+    public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage, ITestContext testContext, Method method) throws IOException {
+        startSession(System.getProperty("OVRURL"), strBrowser, strLanguage, RogersEnums.GroupName.ovr, method);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterTest() {
+        //closeSession();
+    }
+
+    @Test(groups = {"OVR", "RegressionOVR"})
+    public void ovr_Auto_tc_01_SAI_Migration_1P_to_SAI_E2E_Test() throws InterruptedException {
+        getChampLoginPage().logIntoChamp(System.getenv("champLoginUserName"), System.getenv("champLoginPassword"));
+        reporter.reportLogWithScreenshot("Logged into champ successfully");
+        getUniLoginPage().searchWithDealerCode(TestDataHandler.ovrConfigData.getSspdealercode());
+        reporter.reportLogWithScreenshot("Searching with dealer code");
+        getUniLoginPage().selectSSPEnvAndSwitchWindow(TestDataHandler.ovrConfigData.getSspEnvironment());
+        reporter.reportLogWithScreenshot("Select SSP environment");
+        getAccountSearchPage().searchForAccountAndSelectEnv(TestDataHandler.ovrReusableData.getBanNumber(), TestDataHandler.ovrReusableData.getPostalCode(), TestDataHandler.ovrConfigData.getOvrQaEnvironment());
+        reporter.reportLogWithScreenshot("search for account and select environment ");
+        getOvrDashboardPage().clickIgniteLink();
+        reporter.reportLogWithScreenshot("Open IgniteLink from dashboard");
+        getCheckAvailabilityPage().useThisAddress();
+        reporter.reportLogWithScreenshot("Service Availability");
+        getBundleBuilderPage().selectInternet();
+        reporter.reportLogWithScreenshot("Internet Bundle Selected");
+        getBundleBuilderPage().clickloadoffers();
+        getBundleBuilderPage().selectFirstAddToCart();
+        reporter.reportLogWithScreenshot("Product Added to Cart");
+        getBundleBuilderPage().clickCheckout();
+        getBundleBuilderPage().expandPointsToMentionAndContinue();
+        getBundleBuilderPage().clickCheckout();
+        getBundleBuilderPage().selectyescontinue();
+        reporter.reportLogWithScreenshot("Customer Profile");
+        getCustomerProfilePage().clkContinue();
+
+        getBundleBuilderPage().enterdateOfBirth(FormFiller.generateDOBYear(), FormFiller.generateMonth(), FormFiller.generateCalendarDay());
+        getBundleBuilderPage().creditevaluationAndContinue();
+        reporter.reportLogWithScreenshot("Launched theinstall page");
+
+        getBundleBuilderPage().selectExpressProInstall();
+        getBundleBuilderPage().clkTechInstallSlot();
+        getBundleBuilderPage().setMobileNumber();
+        reporter.reportLogWithScreenshot("tech install details");
+        getBundleBuilderPage().clkContinueInstallation();
+        reporter.reportLogWithScreenshot("Billing and Payment Details");
+        getBundleBuilderPage().clkContinueBillingAndPayment();
+        reporter.reportLogWithScreenshot("Order Review Page");
+        reporter.softAssert(getOVROrderReviewPage().verifyOneTimeFees(), "One time Fees is displayed", "One time fees not displayed");
+        reporter.softAssert(getOVROrderReviewPage().verifyMonthlyCharges(), "Monthly Charges is displayed", "Monthly Charges not displayed");
+        getOVROrderReviewPage().clkContinue();
+        reporter.reportLogWithScreenshot("Sign Agreement Page");
+        getOVRAgreementPage().signAgreement();
+        getOVRAgreementPage().clkAgreementCheckbox();
+        getOVRAgreementPage().clkCompleteOrder();
+        reporter.reportLogWithScreenshot("Order Confirmation Page");
+        reporter.hardAssert(getOVROrderConfirmationPage().verifyOrderConfirmation(), "Order Confirmation displayed", "Order not Confirmed");
+        reporter.hardAssert(getOVROrderConfirmationPage().verifyOrderNumberPresent(), "Order number successfully displayed", "Order number not displayed");
+        reporter.hardAssert(getOVROrderConfirmationPage().verifyOneTimeFees(), "One Time Fees Displayed", "One time fees not displayed");
+        reporter.hardAssert(getOVROrderConfirmationPage().verifyMonthlyCharges(), "Monthly Charges displayed", "Monthly charges not displayed");
+
+    }
+
+}
