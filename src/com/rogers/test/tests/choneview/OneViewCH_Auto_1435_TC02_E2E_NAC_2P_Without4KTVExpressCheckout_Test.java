1f1a10627c0cc9816c5e5d6fbe641b25571efffb
aditijainrogers <81329865+aditijainrogers@users.noreply.github.com>
2021-10-20 16:33:59 -0400

    added and updated tests

--- src/com/rogers/test/tests/choneview/OneViewCH_Auto_1435_TC02_E2E_NAC_2P_Without4KTVExpressCheckout_Test.java
+++ src/com/rogers/test/tests/choneview/OneViewCH_Auto_1435_TC02_E2E_NAC_2P_Without4KTVExpressCheckout_Test.java
@@ -11,7 +11,7 @@ import utils.FormFiller;
 import java.io.IOException;
 import java.lang.reflect.Method;
 
-public class OneViewCH_Auto_1435_TC02_E2E_NAC_2P__Without4KTVExpressCheckout_Test extends BaseTestClass{
+public class OneViewCH_Auto_1435_TC02_E2E_NAC_2P_Without4KTVExpressCheckout_Test extends BaseTestClass{
 	 @Test (groups = {"RNAC","RegressionCHOV","SanityCHOV"})
 	    public void endToEndFlowAnonymousCustomerTriplePlayTest(){
 		 reporter.reportLogWithScreenshot("oneview env");
