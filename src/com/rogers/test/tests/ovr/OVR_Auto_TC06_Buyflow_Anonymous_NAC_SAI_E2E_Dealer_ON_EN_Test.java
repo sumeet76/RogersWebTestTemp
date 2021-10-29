a4d54fc9d828a98f92820f358a58a20add86064d
Sam-Ahuja <sameer.ahuja@rci.rogers.ca>
2021-10-29 09:02:52 -0400

    NAC SAI flow test script

---
+++ src/com/rogers/test/tests/ovr/OVR_Auto_TC06_Buyflow_Anonymous_NAC_SAI_E2E_Dealer_ON_EN_Test.java
@@ -0,0 +1,69 @@
+package com.rogers.test.tests.ovr;
+
+import com.rogers.test.base.BaseTestClass;
+import com.rogers.test.helpers.RogersEnums;
+import com.rogers.testdatamanagement.TestDataHandler;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class OVR_Auto_TC06_Buyflow_Anonymous_NAC_SAI_E2E_Dealer_ON_EN_Test extends BaseTestClass {
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"strBrowser", "strLanguage"})
+    public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage, ITestContext testContext, Method method) throws IOException {
+        startSession(System.getProperty("OVRURL"), strBrowser, strLanguage, RogersEnums.GroupName.ovr, method);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterTest() {
+        closeSession();
+    }
+
+    @Test(groups = {"OVR", "RegressionOVR"})
+    public void ovr_Auto_TC06_Anonymous_NAC_SAI_E2E_Dealer_ON_EN_Test() throws InterruptedException {
+        getChampLoginPage().logIntoChamp(System.getenv("champLoginUserName"), System.getenv("champLoginPassword"));
+        reporter.reportLogWithScreenshot("Logged into champ successfully");
+        getUniLoginPage().searchWithDealerCode(TestDataHandler.ovrConfigData.getSspdealercode());
+        reporter.reportLogWithScreenshot("Searching with dealer code");
+        getUniLoginPage().selectSSPEnvAndSwitchWindow(TestDataHandler.ovrConfigData.getSspEnvironment());
+        reporter.reportLogWithScreenshot("Select SSP environment");
+        reporter.reportLogWithScreenshot("Account Search Page");
+        getAccountSearchPage().selectNewCustomerEnv(TestDataHandler.ovrConfigData.getOvrQaEnvironment());
+        reporter.reportLogWithScreenshot("QA Env selected for new customer");
+
+        reporter.reportLogWithScreenshot("Address Availability popup");
+        getRogersIgniteBundlesPage().checkAvailability(TestDataHandler.anonymousData.contactDetails.getAddress());
+        reporter.hardAssert(getRogersIgniteBundlesPage().verifyServiceAvailabilityMessage(),TestDataHandler.anonymousData.contactDetails.getAddress()+" is serviceable",TestDataHandler.anonymousData.contactDetails.getAddress()+" not serviceable");
+        reporter.reportLogWithScreenshot("Service Availability");
+        getRogersIgniteBundlesPage().clkContinue();
+        reporter.hardAssert(getRogersIgniteBundlesPage().verifyAvailableServicesCheckboxes(),"Select Services Customer Wants Displayed","Select Services Customer Wants did not Displayed");
+        reporter.reportLogWithScreenshot("Select Services Customer Wants");
+        getRogersIgniteBundlesPage().clkInternetCheckbox();
+        reporter.reportLogWithScreenshot("Single Play - SAI Selected");
+        getRogersIgniteBundlesPage().clkLoadOffers();
+
+        getRogersIgniteBundlesPage().clickFirstAddToCart();
+        reporter.reportLogWithScreenshot("added to cart");
+        getRogersIgniteBundlesPage().noPortInPopup();
+        reporter.hardAssert(getRogersIgniteBundlesPage().verifyMonthlyFeesInCollapsible(),"Monthly Fees Displayed","Monthly Fees did not Displayed");
+        reporter.reportLogWithScreenshot("Product in cart");
+        //getRogersIgniteBundlesPage().clkCollapse();
+        reporter.hardAssert(getRogersIgniteBundlesPage().verifyProductinCart(),"Product Added to Cart","Failed");
+        reporter.reportLogWithScreenshot("Product Added");
+        reporter.reportLogWithScreenshot("CheckOut for Exchange channels");
+        getRogersIgniteBundlesPage().clkCheckOut();
+        reporter.reportLogWithScreenshot("Cart Summary");
+        getRogersIgniteBundlesPage().clkCheckOutforCartSummary();
+        getRogersIgniteBundlesPage().customerWishtoContinue();
+        reporter.softAssert(getCustomerProfilePage().verifyCustomerProfile(),"Customer Profile","Failed");
+        reporter.reportLogWithScreenshot("Customer Profile");
+        getCustomerProfilePage().clkContinue();
+
+
+
+
+
+    }
+}
