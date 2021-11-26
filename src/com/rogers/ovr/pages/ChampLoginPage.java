5a40641fbe381564380428cd56dbc0f786334239
Sam-Ahuja <sameer.ahuja@rci.rogers.ca>
2021-11-26 16:57:48 -0500

    Removed unused legacy code and refactored code

--- src/com/rogers/ovr/pages/ChampLoginPage.java
+++ src/com/rogers/ovr/pages/ChampLoginPage.java
@@ -8,15 +8,14 @@ import org.openqa.selenium.support.FindBy;
 
 public class ChampLoginPage extends BasePageClass {
 
-    //ChampLoginPage
     @FindBy(name = "loginfmt")
-    WebElement txtemail;
+    WebElement txtEmail;
     @FindBy(id = "idSIButton9")
-    WebElement btnext;
+    WebElement btnNext;
     @FindBy(id = "userNameInput")
-    WebElement txtusername;
+    WebElement txtUserName;
     @FindBy(id = "idSIButton9")
-    WebElement signinbutton;
+    WebElement signInButton;
     @FindBy(xpath = "//input[@type='password']")
     WebElement txtPassword;
     @FindBy(id= "idBtn_Back")
@@ -32,73 +31,16 @@ public class ChampLoginPage extends BasePageClass {
     WebElement corpLoginSkip2faSetup;
 
 
-    //UniLoginPage
-    @FindBy(xpath = "//*[@id='dsa-accordion-panel-0-body-0']/div/raap-dealer-code-auth/section/form/ds-form-field/div/div[1]")
-    WebElement eledealercode;
-
-    @FindBy(id = "ds-form-input-id-0")
-    WebElement txtdealercode;
-
-    @FindBy(xpath = "//*[text()=' Submit ']")
-    WebElement btnSubmit;
-//-----
-
-    //AccountSearchPage
-    //*[@id='dsa-accordion-panel-0-body-0']/div/raap-dealer-code-auth/section/form/ds-form-field/div/div[1]
-    @FindBy(xpath = "//input[@value='Account Number or Phone Number']")
-    WebElement txtbannumber;
-    @FindBy(xpath = "//*[@id='postalcode']")
-    WebElement txtpostalcode;
-
-    @FindBy(id = "searchCustomerBtn")
-    WebElement btnSearch;
-
-
-    @FindBy(id = "viewUTEMyRogersLink1")
-    WebElement drpdwnPlsSelect;
-
-    @FindBy(xpath = "//*[text()='CONTINUE']")
-    WebElement btncontnu_customerAuthen;
-
-    @FindBy(xpath = "//*[text()='Continue'] ")
-    WebElement btncontnu;
-
-
-
-
-    @FindBy(xpath = "//app-dashboard/div[2]/div/div[2]/a/span")
-    WebElement lnkIgnite;
-
-    @FindBy(xpath = "//span[text()='Use this address']")
-    WebElement btnusethisAddrs;
-
-
-    @FindBy(xpath = "//a[@role='button' and @aria-label='quick-links']")
-    WebElement lnkExpandFooter;
-
-    @FindBy(partialLinkText = "LNP Eligibility")
-    WebElement lnklnpEligibility;
-
-    @FindBy(xpath="//*[@id='top-right-links']/li/a")
-    WebElement lnkfrenchlang ;
-
-    @FindBy(xpath="//*[@id='content_inner']/div/h4")
-    WebElement eleVerifyPortEligibity ;
-
-
-    String sspbtn_xpath = "//span[contains(text(),'SSP Rogers SSP_ENV')]";
-
-
     public ChampLoginPage(WebDriver driver) {
         super(driver);
     }
 
     public void logIntoChamp(String email, String password) throws InterruptedException {
 
-        getReusableActionsInstance().getWhenVisible(txtemail).sendKeys(email);
-        getReusableActionsInstance().getWhenVisible(btnext).click();
+        getReusableActionsInstance().getWhenVisible(txtEmail).sendKeys(email);
+        getReusableActionsInstance().getWhenVisible(btnNext).click();
         getReusableActionsInstance().getWhenVisible(txtPassword).sendKeys(password);
-        getReusableActionsInstance().getWhenVisible(signinbutton).click();
+        getReusableActionsInstance().getWhenVisible(signInButton).click();
         if(getReusableActionsInstance().isElementVisible(staySignedInNO, 10)){
             getReusableActionsInstance().getWhenVisible(staySignedInNO).click();
         }
@@ -107,8 +49,8 @@ public class ChampLoginPage extends BasePageClass {
 
     public void logIntoCorpChamp(String email, String password) throws InterruptedException {
 
-        getReusableActionsInstance().getWhenVisible(txtemail).sendKeys(email);
-        getReusableActionsInstance().getWhenVisible(btnext).click();
+        getReusableActionsInstance().getWhenVisible(txtEmail).sendKeys(email);
+        getReusableActionsInstance().getWhenVisible(btnNext).click();
         getReusableActionsInstance().getWhenVisible(txtPassword).sendKeys(password);
         getReusableActionsInstance().getWhenVisible(corpSignInBtn).click();
         getReusableActionsInstance().getWhenVisible(corpLoginNextBtn).click();
