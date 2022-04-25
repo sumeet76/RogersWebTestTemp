514849372f6432688c8de8fcc3e81d01f5b584ad
Gosamit <Amit.Goswami1@rci.rogers.ca>
2022-04-25 13:23:39 -0400

    change totest data handler file

--- src/com/rogers/testdatamanagement/TestDataHandler.java
+++ src/com/rogers/testdatamanagement/TestDataHandler.java
@@ -336,6 +336,7 @@ public class TestDataHandler {
 	public static AccountData Tv_Dashboard_Validation;
 	public static AccountData Internet_Dashboard_Validation;
 	public static AccountData RHP_Dashboard_Validation;
+	public static AccountData RHPAddOns_ValidateConfigureCallerIdAndDirectoryListingPage;
 	public static MigrationData TargetedMigration_Delivery_by_Appointment_3pto3p_MonthlyCharges;
 
 
@@ -726,6 +727,7 @@ public class TestDataHandler {
 		Validation_of_Things_to_know_copy_Directory_Listing_Home_Phone_Personalisation=YamlHandler.getMigrationData("Validation_of_Things_to_know_copy_Directory_Listing_Home_Phone_Personalisation");
 		ValidationofNotificationAlertBackbutton=YamlHandler.getMigrationData("Validation_of_Notification_Alert_Back_Button_on_Migration_Flow");
 		TargetedMigration_Delivery_by_Appointment_3pto3p_MonthlyCharges=YamlHandler.getMigrationData("TargetedMigration_3P_to_3P_Delivery_by_Appointment_MonthlyCharges");
+		RHPAddOns_ValidateConfigureCallerIdAndDirectoryListingPage= YamlHandler.getOVAccountData("RHPAddOns_ValidateConfigureCallerIdAndDirectoryListingPage");
 
 	}
 
