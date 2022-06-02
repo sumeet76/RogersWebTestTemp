245ff3a5166ba45a33a9b5b29105a0be536fa24a
Gosamit <Amit.Goswami1@rci.rogers.ca>
2022-06-02 16:25:50 -0400

    2nd june

--- src/com/rogers/test/tests/choneview/OneViewCH_Auto_OVS3_Validate_IPTV_dashboard_for_the_account_having_Mulitpls_STB_EN.java
+++ src/com/rogers/test/tests/choneview/OneViewCH_Auto_OVS3_Validate_IPTV_dashboard_for_the_account_having_Mulitpls_STB_EN.java
@@ -10,9 +10,9 @@ import org.testng.annotations.*;
 import java.io.IOException;
 import java.lang.reflect.Method;
 
-public class OneViewCH_Auto_2557_TC02_IPTV_Dasboad_Mulitpls_STB_Test extends BaseTestClass {
-    @Test (groups = {"Regression"})
-    public void oneViewCH_Auto_2557_TC02_IPTV_Dasboad_Mulitpls_STB_Test() {
+public class OneViewCH_Auto_OVS3_Validate_IPTV_dashboard_for_the_account_having_Mulitpls_STB_EN extends BaseTestClass {
+    @Test
+    public void oneViewCH_Auto_OVS3_Validate_IPTV_dashboard_for_the_account_having_Mulitpls_STB_EN(){
         reporter.reportLogWithScreenshot("One view is launched");
         getEnvironmentSelectionPage().launchOneView(TestDataHandler.IPTVDasboad_MulitplsSTB.getAccountNo(),TestDataHandler.IPTVDasboad_MulitplsSTB.getContactID());
         getAccountOverViewPage().clickIgniteTVBadge();
@@ -20,19 +20,16 @@ public class OneViewCH_Auto_2557_TC02_IPTV_Dasboad_Mulitpls_STB_Test extends Bas
         reporter.reportLogWithScreenshot("select TV Badage");
 //        getRogersAccountOverviewPage().scrollToBottomOfPage();
         getAccountOverViewPage().verifydisplayedMulitplsSTB();
-
         reporter.reportLogWithScreenshot("Test Passed");
     }
-
-        @BeforeMethod(alwaysRun=true)
-        @Parameters({"strBrowser","strLanguage"})
-        public void beforeTest(@Optional("chrome")String strBrowser, @Optional("en")String strLanguage, ITestContext
-        testContext, Method method)throws ClientProtocolException, IOException {
-            startOVSession(System.getProperty("OVUrl"),strBrowser,strLanguage, RogersEnums.GroupName.connectedhome_oneview.toString().toLowerCase().trim(),"","","","",method);
-        }
-
-        @AfterMethod(alwaysRun=true)
-        public void afterTest(){
-            closeSession();
+    @BeforeMethod(alwaysRun=true)
+    @Parameters({"strBrowser","strLanguage"})
+    public void beforeTest(@Optional("chrome")String strBrowser, @Optional("en")String strLanguage, ITestContext
+            testContext, Method method)throws ClientProtocolException, IOException {
+        startOVSession(System.getProperty("OVUrl"),strBrowser,strLanguage, RogersEnums.GroupName.connectedhome_oneview.toString().toLowerCase().trim(),"","","","",method);
+    }
+    @AfterMethod(alwaysRun=true)
+    public void afterTest(){
+        closeSession();
     }
 }
