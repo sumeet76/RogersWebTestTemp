13123e0de90b2982ac8dca8c5f8daead2db2a60d
ManpreetKaur-Rogers <Manpreet.Kaur3@rci.rogers.com>
2022-08-10 14:37:58 -0400

    IDE Clean up activities (TC063-TC108)

---
+++ src/com/rogers/test/tests/connectedhome/desktop/RogersCH_Auto_TC056_IgniteNAC_CartAbandon_CancelCartatProfileCreationPage_StartOver_newBeforeYouGoModal_saveCartAndRetrieve_ATLTest.java
@@ -0,0 +1,105 @@
+package com.rogers.test.tests.connectedhome.desktop;
+
+import com.rogers.test.base.BaseTestClass;
+import com.rogers.test.helpers.RogersEnums;
+import com.rogers.testdatamanagement.TestDataHandler;
+import org.apache.http.client.ClientProtocolException;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+
+/**
+ * This class contains the test method to test Legacy Internet Offer Buy flow for Rogers.com
+ *
+ * @author chinnarao.vattam
+ * <p>
+ * Test steps:
+ * 1.Launch Rogers SAI Tupelo URL in QA Env and click on get it now and enter address and click on continue
+ * 2. Click on continue
+ * 3. Choose Internet, SmartStream checkbox and click on Load offers
+ * 4. Add 1 STB and click on Add to cart
+ * 5. Click on Continue
+ * 6. Click on Checkout
+ * 7. Click on Yes, continue
+ * 8. Click on Continue
+ * 9. Enter DOB, valid ID details and click on continue
+ * 10. Choose Installation type as enhanced self install and click on continue
+ * 11. Click on Continue
+ * 12. Click on Submit
+ **/
+
+public class RogersCH_Auto_TC056_IgniteNAC_CartAbandon_CancelCartatProfileCreationPage_StartOver_newBeforeYouGoModal_saveCartAndRetrieve_ATLTest extends BaseTestClass {
+
+    @Test(groups = {"RegressionCH", "RogersCartAbandon"})
+    public void rogersCH_Auto_TC056_IgniteNAC_CartAbandon_CancelCartatProfileCreationPage_StartOver_newBeforeYouGoModal_saveCartAndRetrieve_ATL() {
+        reporter.reportLogWithScreenshot("Launched the Easy Login Page");
+        getRogersHomePage().clkTVBundle();
+        reporter.hardAssert(getRogersHomePage().verifyIgnitepage(), "Ignite page has Launched", "Ignite page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the IgniteTV page");
+        getRogersHomePage().clkServiceability();
+        reporter.hardAssert(getRogersIgniteTVBuyPage().verifyBundlesPage(), "Bundles Page has launched", "Bundles Page has not launched");
+        getRogersIgniteTVBuyPage().clkHomephone();
+        reporter.reportLogWithScreenshot("Checked the Home Phone");
+        getRogersIgniteTVBuyPage().selectSolarisStarterPackage();
+
+        reporter.reportLogWithScreenshot("Launched the customer availability check popup");
+        String strAddressLine1 = TestDataHandler.tc01_02_03_IgniteTVAccount.getAccountDetails().getAddress().get("line1");
+        String strAddressLine2 = TestDataHandler.tc01_02_03_IgniteTVAccount.getAccountDetails().getAddress().get("line2");
+        getRogersHomePage().setIgniteAddressLookup(strAddressLine1 + ", " + strAddressLine2);
+        reporter.reportLogWithScreenshot("Address entered");
+        getRogersHomePage().clkIgniteAddressLookupSubmit();
+
+        reporter.hardAssert(getRogersIgniteTVBuyPage().verifyOffersPage(), "Offers Page has launched", "Offers Page has not launched");
+        getRogersIgniteTVBuyPage().selectStarterPackageMonthToMonthTypeOfContract();
+        reporter.reportLogWithScreenshot("Selected month-to-month term contract");
+        getRogersIgniteTVBuyPage().selectSolarisStarterPackage();
+
+        reporter.hardAssert(getRogersHomePhoneSelectionPage().verifyPortInOutPage(), "Port-InOut page has Launched", "Port-InOut page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the home phone selection page");
+        getRogersHomePhoneSelectionPage().clkSkipforNewNumber();
+        reporter.reportLogWithScreenshot("Launched the Home phone add-on page");
+        getRogersIgniteTVBuyPage().clkHomePhone();
+
+        reporter.hardAssert(getRogersIgniteTVBuyPage().verify4KTV(), "4KTV radio button is availabe", "4KTV radio button is not availabe");
+        reporter.reportLogWithScreenshot("Launched the cart summary page");
+        getRogersIgniteTVBuyPage().set4KTV();
+        reporter.reportLogWithScreenshot("4k TV selected");
+        getRogersIgniteTVBuyPage().clkCheckout();
+
+        reporter.hardAssert(getRogersIgniteTVProfileCreationPage().verifyProfilePage(), "Profile page has Launched", "Profile page has not Launched");
+        reporter.reportLogWithScreenshot("Launched the create profile page");
+        getRogersIgniteTVProfileCreationPage().setEmail();
+        getRogersIgniteTVProfileCreationPage().setFirstname();
+        getRogersIgniteTVProfileCreationPage().setLastName();
+        getRogersIgniteTVProfileCreationPage().setPhone();
+        reporter.reportLogWithScreenshot("Entered the details on Profile page");
+        getRogersIgniteTVProfileCreationPage().clkSubmitCancel();
+        reporter.hardAssert(getRogersIgniteTVBuyPage().verifyEmailModal(), "Email Modal is availabe", "EmailModal is not availabe");
+        reporter.reportLogWithScreenshot("Launched the Email Modal");
+        getRogersIgniteTVBuyPage().clickStartOver();
+        
+        reporter.hardAssert(getRogersIgniteTVBuyPage().verifyOffersPage(), "Offers Page has launched", "Offers Page has not launched");
+    }
+
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"strBrowser", "strLanguage"})
+    //legacyAnonymous
+    public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage, ITestContext testContext, Method method) throws ClientProtocolException, IOException {
+        startSession(System.getProperty("QaUrl"), strBrowser, strLanguage, RogersEnums.GroupName.connectedhome_igniteanonymous, method);
+        // xmlTestParameters = new HashMap<String, String>(testContext.getCurrentXmlTest().getAllParameters());
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void afterTest() {
+        closeSession();
+    }
+
+
+}
+
+
---
+++ src/com/rogers/test/tests/connectedhome/mobile/Mobile_RogersCH_Auto_TC006_SolarisTVCx_TVChannelsSwapTest.java
@@ -0,0 +1,114 @@
+package com.rogers.test.tests.connectedhome.mobile;
+
+import com.rogers.test.base.BaseTestClass;
+import com.rogers.test.helpers.RogersEnums;
+import com.rogers.testdatamanagement.TestDataHandler;
+import org.apache.http.client.ClientProtocolException;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+
+/**
+ * This class contains the test method to test the TV channels swap functionality for Rogers.com   
+ * 
+ * @author chinnarao.vattam
+ * 
+ * Test steps:
+ *
+ *1. Launch the Rogers.com url.
+ *2. Login with valid credentials (Solaris account credentials with TV ).
+ *3. Click on TV badge.
+ *4. Click on Exchange Flex Channels button.
+ *5. In Select Channels to remove section, select a change. 
+ *6. Click  on Select in the pop up.
+ *7. In Select Channels to add section, select one  channel.
+ *8. Click  on Select in the pop up.
+ *9. Check the "Sure, lets do a Credit Evaluation" check box and click on Submit.
+ *10. Scroll down the page and click on Confirm Exchange.
+ *11. Click on Ok.
+ *
+ **/
+
+public class Mobile_RogersCH_Auto_TC006_SolarisTVCx_TVChannelsSwapTest extends BaseTestClass {
+	
+    @Test(groups = {"MobileRegressionCH"})
+   public void mobile_RogersCH_Auto_TC006_SolarisTVCx_TVChannelsSwap() {
+     reporter.reportLogWithScreenshot("Launched the SignIn popup");
+     if (getRogersLoginPage().isOverlayContainerDisplayed()) {
+      reporter.reportLogWithScreenshot("Select Continue in browser.");
+      getRogersLoginPage().clkContinueInBrowser();
+      reporter.reportLogWithScreenshot("Continue in Browser Selected");
+     }
+     getRogersLoginPage().setUsernameMobile(TestDataHandler.tcm06_IgniteTVAccount.getUsername());
+     getRogersLoginPage().setPasswordMobile(TestDataHandler.tcm06_IgniteTVAccount.getPassword());
+     reporter.reportLogWithScreenshot("Enter the account credentails");
+     getRogersLoginPage().clkSignInIFrame();
+     reporter.hardAssert(!getRogersLoginPage().verifyLoginFailMsgIframe(),"Login Successful","Login Failed");
+     getRogersLoginPage().clkSkipIFrame();
+     reporter.reportLogWithScreenshot("Skip popup");
+     // reporter.hardAssert(getRogersAccountOverviewPage().verifyLoginSuccessWelcome(),"Launched the Account Page","Account Page hasn't launched");
+     reporter.reportLogWithScreenshot("Launched the Account Page");
+     getRogersSolarisTVDashboardPage().clkTVBadgeMobile();
+     reporter.reportLogWithScreenshot("Launched the TV dash board");
+     getRogersSolarisTVDashboardPage().clkChangeFlexChannelsMobile();
+     reporter.reportLogWithScreenshot("TV channels pannel has displayed");
+     getRogersSolarisChannelsExchangePage().verifyExchangeChannelPage();
+     reporter.reportLogWithScreenshot("TV channels exchange page has launched");
+     getRogersSolarisChannelsExchangePage().verifyChannelsPannel();
+     reporter.reportLogWithScreenshot("TV channels pannel has displayed");
+
+     getRogersSolarisChannelsExchangePage().swapChannelOutMobile(TestDataHandler.tcm06_IgniteTVAccount.accountDetails.getSwapOutChannelOne());
+     reporter.reportLogWithScreenshot("selected the Swap-out Channel one");
+     getRogersSolarisChannelsExchangePage().clkRemoveChannel();
+     reporter.reportLogWithScreenshot("removed the Swap-out Channel one");
+     getRogersSolarisChannelsExchangePage().swapChannelInMobile(TestDataHandler.tcm06_IgniteTVAccount.accountDetails.getSwapInChannelOne());
+     reporter.reportLogWithScreenshot("Selected the Swap-in Channel one");
+     getRogersSolarisChannelsExchangePage().clkAddChannelDifferentLogic();
+     reporter.reportLogWithScreenshot("Added the Swap-in Channel one");
+
+     getRogersSolarisChannelsExchangePage().swapChannelOutMobile(TestDataHandler.tcm06_IgniteTVAccount.accountDetails.getSwapOutChannelTwo());
+     reporter.reportLogWithScreenshot("selected the Swap-out Channel two");
+     getRogersSolarisChannelsExchangePage().clkRemoveChannel();
+     reporter.reportLogWithScreenshot("removed the Swap-out Channel two");
+     getRogersSolarisChannelsExchangePage().swapChannelInMobile(TestDataHandler.tcm06_IgniteTVAccount.accountDetails.getSwapInChannelTwo());
+     reporter.reportLogWithScreenshot("Selected the Swap-in Channel two");
+     getRogersSolarisChannelsExchangePage().clkAddChannelDifferentLogic();
+     reporter.reportLogWithScreenshot("Added the Swap-in Channel two");
+
+     getRogersSolarisChannelsExchangePage().swapChannelOutMobile(TestDataHandler.tcm06_IgniteTVAccount.accountDetails.getSwapOutChannelThree());
+     reporter.reportLogWithScreenshot("selected the Swap-out Channel three");
+     getRogersSolarisChannelsExchangePage().clkRemoveChannel();
+     reporter.reportLogWithScreenshot("removed the Swap-out Channel three");
+     getRogersSolarisChannelsExchangePage().swapChannelInMobile(TestDataHandler.tcm06_IgniteTVAccount.accountDetails.getSwapInChannelThree());
+     reporter.reportLogWithScreenshot("Selected the Swap-in Channel three");
+     getRogersSolarisChannelsExchangePage().clkAddChannelDifferentLogic();
+     reporter.reportLogWithScreenshot("Added the Swap-in Channel three");
+     getRogersSolarisChannelsExchangePage().clkConfirmSwapMobile();
+
+     reporter.hardAssert(getRogersSolarisChannelsExchangePage().verifyExchangeSuccess(),"Swap Success","Swap failed");
+     reporter.reportLogWithScreenshot("Swap Success popup has launched");
+     getRogersSolarisChannelsExchangePage().clkReturnToDashbaord();
+     reporter.reportLogWithScreenshot("Tv Dashboard has launched");
+	}
+	
+
+@BeforeMethod (alwaysRun=true) @Parameters({ "strBrowser", "strLanguage"})
+//login flow
+public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage,  ITestContext testContext, Method method) throws ClientProtocolException, IOException {
+	// xmlTestParameters = new HashMap<String, String>(testContext.getCurrentXmlTest().getAllParameters());
+	startSession(System.getProperty("QaUrl"), strBrowser,strLanguage,RogersEnums.GroupName.connectedhome_login, method);
+}
+
+@AfterMethod(alwaysRun = true)
+public void afterTest() {
+	closeSession();
+}
+
+
+
+}
+
+
