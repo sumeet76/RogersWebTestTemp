6c980c761a7c0e24c24a8d5653d8b427cebe5408
ManpreetKaur-Rogers <Manpreet.Kaur3@rci.rogers.com>
2022-10-03 13:34:35 -0400

    TC022 xpath update for 1 gbps packag selection

--- src/com/rogers/pages/RogersInternetDashboardPage.java
+++ src/com/rogers/pages/RogersInternetDashboardPage.java
@@ -532,8 +532,8 @@ public class RogersInternetDashboardPage extends BasePageClass {
 	 */
 	public void selectSolarisInternetPackage(String strPackageNameEn,String strPackageNameFr) {
 		getReusableActionsInstance().staticWait(5000);
-		By packageNameLocator = By.xpath("//span[contains(text(),'" + strPackageNameEn+ "') or contains(normalize-space(text()),'" + strPackageNameFr +"')]/ancestor::div[@class='change-internet-bundle-tile__row']//button[contains(@aria-label,'" + strPackageNameEn+ "') or contains(@aria-label, '" + strPackageNameEn+ "')]/span");
-		//By packageNameLocator = By.xpath("//span[contains(text(),'" + strPackageNameEn+ "') or contains(normalize-space(text()),'" + strPackageNameFr +"')]/ancestor::div[@class='change-internet-bundle-tile__row']//button[contains(@aria-label,'Add Rogers Ignite') or contains(@aria-label, 'Élan Premier')]/span");
+		//By packageNameLocator = By.xpath("//span[contains(text(),'" + strPackageNameEn+ "') or contains(normalize-space(text()),'" + strPackageNameFr +"')]/ancestor::div[@class='change-internet-bundle-tile__row']//button[contains(@aria-label,'" + strPackageNameEn+ "') or contains(@aria-label, '" + strPackageNameEn+ "')]/span");
+		By packageNameLocator = By.xpath("//div[contains(@rchapiexposer,'offerPrice')]//button[contains(@aria-label, '1 Gbps')]/span");
 		getReusableActionsInstance().getWhenReady(packageNameLocator, 90);
 		WebElement pkg = getDriver().findElement(packageNameLocator);
 		getReusableActionsInstance().getWhenReady(pkg,60).click();
