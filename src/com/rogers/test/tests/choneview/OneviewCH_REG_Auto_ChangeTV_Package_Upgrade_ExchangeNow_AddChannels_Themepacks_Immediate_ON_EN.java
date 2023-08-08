5befd251b7bc83fdc874323ebe31db22f81a29a2
RCI\Jasmeen.Kaur <jasmeen.kaur@rci.rogers.ca>
2023-08-08 16:24:23 -0400

    added new test case and made changes to existing ones

--- src/com/rogers/test/tests/choneview/OneviewCH_REG_Auto_ChangeTV_Package_Upgrade_ExchangeNow_AddChannels_Themepacks_Immediate_ON_EN.java
+++ src/com/rogers/test/tests/choneview/OneviewCH_REG_Auto_ChangeTV_Package_Upgrade_ExchangeNow_AddChannels_Themepacks_Immediate_ON_EN.java
@@ -19,7 +19,7 @@ import java.lang.reflect.Method;
 
 public class OneviewCH_REG_Auto_ChangeTV_Package_Upgrade_ExchangeNow_AddChannels_Themepacks_Immediate_ON_EN extends BaseTestClass {
 	@Test (groups = {"RegressionCHOV"})
-    public void oneviewCH_REG_Auto_TC002_ChangeTV_INT_Package_Downgrade_ExchangeNow_AddChannels_Themepacks_Immediate_ON_EN() {
+    public void oneviewCH_REG_Auto_ChangeTV_Package_Upgrade_ExchangeNow_AddChannels_Themepacks_Immediate_ON_EN() {
 		getEnvironmentSelectionPage().launchOneView(TestDataHandler.Change_TV_Channel_Themepack_Immediate_Downgrade.accountDetails.getBan(),TestDataHandler.Change_TV_Channel_Themepack_Immediate_Downgrade.getContactID());
 		reporter.reportLogWithScreenshot("OneView Interface has Launched");
 		getAccountOverViewPage().enterDealerCodeDialogue();
