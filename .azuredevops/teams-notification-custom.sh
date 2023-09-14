6c012ba091f7145bd6dfbb3b796702962ca78fa5
aarenzon <alex.arenzon@rci.rogers.ca>
2023-09-14 12:15:34 -0400

    1215

--- .azuredevops/teams-notification-custom.sh
+++ .azuredevops/teams-notification-custom.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 teams="${1}"
 
@@ -81,10 +82,13 @@ cat > ${filename}.json <<'endmsg'
 endmsg
 
 url_encoded_project_name=$(rawurlencode "${SYSTEM_TEAMPROJECT}")
-build_url=$(curl -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${SYSTEM_COLLECTIONURI}${url_encoded_project_name}/_apis/build/builds/${BUILD_BUILDID}?api-version=6.0" | jq -r '._links.web.href')
-timeline_url=$(curl -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${SYSTEM_COLLECTIONURI}${url_encoded_project_name}/_apis/build/builds/${BUILD_BUILDID}?api-version=6.0" | jq -r '._links.timeline.href')
+echo "url_encoded_project_name: $url_encoded_project_name"
+build_url=$(curll -s -S -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${SYSTEM_COLLECTIONURI}${url_encoded_project_name}/_apis/build/builds/${BUILD_BUILDID}?api-version=6.0" | jq -r '._links.web.href')
+echo "build_url: $build_url"
+timeline_url=$(curl -s -S -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${SYSTEM_COLLECTIONURI}${url_encoded_project_name}/_apis/build/builds/${BUILD_BUILDID}?api-version=6.0" | jq -r '._links.timeline.href')
+echo "timeline_url: $timeline_url"
 
-readarray -t stage_results << (curl -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${timeline_url}" | jq -r '.records[] | select(.state=="completed" and .type=="Stage" and .identifier!="environment" and .identifier!="post") | .result')
+readarray -t stage_results < <(curl -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${timeline_url}" | jq -r '.records[] | select(.state=="completed" and .type=="Stage" and .identifier!="environment" and .identifier!="post") | .result')
 
 build_result="Unknown"
 
