bf5621917b8b2f86d88c45685efc44e5380c05c4
aarenzon <alex.arenzon@rci.rogers.ca>
2023-09-14 16:48:01 -0400

    447

--- .azuredevops/teams-notification-custom.sh
+++ .azuredevops/teams-notification-custom.sh
@@ -88,7 +88,7 @@ timeline_url=$(curl -s -S -X GET -H "ContentType: application/json" -H "Authoriz
 
 echo "timeline_url: $timeline_url"
 
-readarray -t stage_results < <(curl -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${timeline_url}" | jq -r '.records[] | select(.state=="completed" and .type=="Stage" and .identifier!="environment" and .identifier!="post") | .result')
+# readarray -t stage_results < <(curl -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${timeline_url}" | jq -r '.records[] | select(.state=="completed" and .type=="Stage" and .identifier!="environment" and .identifier!="post") | .result')
 
 curl -s -S -X GET -H "ContentType: application/json" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${timeline_url}" > result.json
 
